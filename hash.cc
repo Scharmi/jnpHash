fe60fe94b5c94b8ae5703df71321c18996ee0e73
Scharmi <your_email@example.com>
2022-11-04 22:10:06 +0100

    Update obu plików

--- hash.cc
+++ hash.cc
@@ -4,53 +4,101 @@
 #include <vector>
 #include <unordered_set>
 #include <unordered_map>
+#include <cstddef>
 #include "hash.h"
-
+ 
+#define map_find ((mapa().find(id))->second) //wskazuje set od id
+ 
 namespace jnp1 {
-    unsigned long &id() {
-        static unsigned long id = 0;
-        return id;
-    }
+    unsigned long &id_global() {
+        static unsigned long id_global = 0;
+        return id_global;
+    };
     struct hf {
         hash_function_t funkcja;
         hf(hash_function_t funkcjaZWejscia) {
             funkcja = funkcjaZWejscia;
         }
-        size_t operator () (const std::vector<uint32_t> &wektor) const {
+        size_t operator () (const std::vector<uint64_t> &wektor) const {
             return funkcja(wektor.data(), wektor.size());
         }
-    }
-    std::unordered_map<unsigned long, unordered_set<std::vector<uint32_t>, hf> &mapa() {
-        static std::unordered_map<unsigned long, unordered_set<std::vector<uint32_t>, hf> mapa;
+    };
+    std::unordered_map< unsigned long, std::unordered_set< std::vector<uint64_t>, hf >  > &mapa() {
+        static std::unordered_map<unsigned long, std::unordered_set<std::vector<uint64_t>, hf> > mapa;
         return mapa;
+    };
+    std::vector<uint64_t> arr_to_vec(uint64_t const * seq, size_t size){
+        std::vector<uint64_t> vec;
+        for(size_t i = 0 ; i < size ; i++)
+            vec.push_back(seq[i]);
+        return vec;
     }
-    //Nieprzetestowane jeszcze
+    // //Nieprzetestowane jeszcze
     unsigned long hash_create(jnp1::hash_function_t x) {
-        std::unordered_set<std::vector<uint32_t>, hf> zbiur(0, hf(x));
-        mapa().insert(id, zbiur);
-        id()++;
-        return id();
+        std::unordered_set<std::vector<uint64_t>, hf> zbiur(0, hf(x));
+        std::unordered_set<int> secior(0);
+        mapa().insert({id_global(), zbiur});
+        id_global()++;
+        return id_global() - 1;
+        //niescislosc byla bo mapa na np. set ma id 0, a bylo zwracane 1
     }
-    void hash_delete(unsigned long) {
-
+    void hash_delete(unsigned long id) {
+        mapa().erase(id);
     }
-    size_t hash_size(unsigned long) {
-
+    size_t hash_size(unsigned long id) {
+        if(mapa().find(id) == mapa().end()){
+            //nie ma klucza
+            return 0;
+        }
+        else{
+            return map_find.size();
+        }
     }
-    bool hash_insert(unsigned long, uint64_t const *, size_t) {
-
+    bool hash_insert(unsigned long id, uint64_t const * seq, size_t size) {
+        if(seq == NULL || mapa().find(id) == mapa().end() || size == 0){
+            return false;
+        }
+        else{
+            if(hash_test(id, seq, size) == true){
+                return false;
+            }
+            else{
+                map_find.insert(arr_to_vec(seq, size));
+                return true;
+            }
+            //error tylko gdy cos nie powiedzie lub set ma juz ten hash(ciag)
+        }
     }
-    bool hash_remove(unsigned long, uint64_t const *, size_t) {
-
+    bool hash_remove(unsigned long id, uint64_t const * seq, size_t size) {
+        if(seq == NULL || mapa().find(id) == mapa().end() || size == 0){
+            return false;
+        }
+        else{
+            if(hash_test(id, seq, size) == false){
+                return false;
+            }
+            else{
+                map_find.erase(arr_to_vec(seq, size));
+                return true;
+            }
+            //error tylko gdy cos nie powiedzie lub set nie ma tego hash(ciag)
+            return true;
+        }
     }
-    void hash_clear(unsigned long) {
-
+    void hash_clear(unsigned long id) {
+        if(mapa().find(id) != mapa().end()){
+            map_find.clear();
+        }
     }
-    bool hash_test(unsigned long, uint64_t const *, size_t) {
-
+    bool hash_test(unsigned long id, uint64_t const * seq, size_t size) {
+        if(seq == NULL || mapa().find(id) == mapa().end() || size == 0){
+            return false;
+        }
+        else{
+            if(map_find.find(arr_to_vec(seq, size))  == map_find.end())
+                return false;
+            else
+                return true;
+        }
     }
-}
-
-int main() {
-    std::cout << "Hello World\n";
 }
--- hash.h
+++ hash.h
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <iostream>
 #ifdef __cplusplus 
 namespace jnp1 {
 extern "C" {
@@ -10,11 +11,11 @@ extern "C" {
 typedef uint64_t (*hash_function_t) (uint64_t const*, size_t);
 unsigned long hash_create(hash_function_t);
 void hash_delete(unsigned long id);
-size_t hash_size(unsigned long);
-bool hash_insert(unsigned long, uint64_t const *, size_t);
-bool hash_remove(unsigned long, uint64_t const *, size_t);
-void hash_clear(unsigned long);
-bool hash_test(unsigned long, uint64_t const *, size_t);
+size_t hash_size(unsigned long id);
+bool hash_insert(unsigned long id, uint64_t const * seq, size_t size);
+bool hash_remove(unsigned long id, uint64_t const * seq, size_t size);
+void hash_clear(unsigned long id);
+bool hash_test(unsigned long id, uint64_t const * seq, size_t size);
 #ifdef __cplusplus 
 }
 }
